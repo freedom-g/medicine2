c39a4cef0da877ec6bcf19f0ba1c77a58f656c39
freedom-g <15502283132@163.com>
2020-02-19 15:10:46 +0800

    门户修改service接口和实现类

--- pharmacy-portal/src/main/java/com/portal/service/ContentService.java
+++ pharmacy-portal/src/main/java/com/portal/service/ContentService.java
@@ -1,8 +1,17 @@
 package com.portal.service;
 
+import com.medince.pojo.Content;
+import com.medince.pojo.MedicineMessage;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2020/2/1 0001.
  */
 public interface ContentService {
-    public String getContentList();
+    //查询大广告信息
+    public List<Content> getContentList();
+
+    //查询促销商品信息
+    public List<MedicineMessage> getMessageStatus();
 }
